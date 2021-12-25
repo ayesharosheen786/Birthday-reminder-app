6fabf7299af769f132adfd92432fa7bdecdda715
ayesharosheen786 <ayesharosheen5@gmail.com>
2021-12-25 13:08:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { data } from './Birthday-data'
 
 function App() {
+  const [people, setPeople] = useState(data)
+
+  const removePerson = (id) => {
+    let newPerson = people.filter((person) => person.id !== id)
+    setPeople(newPerson)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h3
+        style={{
+          backgroundColor: '#fff',
+          color: '#313174',
+          textAlign: 'left',
+          fontSize: '36px',
+          marginTop: '70px'
+        }}
+      >
+        {people.length} birthdays today
+      </h3>
+      {people.map((person) => {
+        const { id, name, image } = person
+
+        return (
+          <div className='container' key={id}>
+            <div className='avatar'>
+              <img src={image} alt={name} />
+              <h3>{name}</h3>
+            </div>
+            <div className='button'>
+              <button className='btn' onClick={() => removePerson(id)}>
+                Dismiss
+              </button>
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
 }
 
-export default App;
+export default App
+
+
---
+++ src/Birthday-data.js
@@ -0,0 +1,76 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Vijay kumar',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/7/7e/Virat_Kohli.jpg'
+    },
+    {
+      id: 2,
+      name: 'John ',
+      image:
+        'https://images.pexels.com/photos/3031397/pexels-photo-3031397.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    },
+    {
+      id: 3,
+      name: 'Rahul Jain',
+      image:
+        'https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    },
+    {
+      id: 4,
+      name: 'Priya Verma',
+      image:
+        'https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    },
+    {
+      id: 5,
+      name: 'Ronny ',
+      image:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    },
+    {
+      id: 6,
+      name: 'Sandeep Kumar',
+      image:
+        'https://images.pexels.com/photos/1396538/pexels-photo-1396538.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    },
+    {
+      id: 7,
+      name: 'Lucy',
+      image:
+        'https://images.pexels.com/photos/3754438/pexels-photo-3754438.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    },
+    {
+      id: 8,
+      name: 'Raj',
+      image:
+        'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    },
+    {
+      id:9,
+      name:'Rohit Sharma',
+      image:"https://timesofindia.indiatimes.com/photo/69257289.cms"
+    },
+    {
+      id:10,
+      name:'Virat Kohli',
+
+      image:"https://upload.wikimedia.org/wikipedia/commons/7/7e/Virat_Kohli.jpg"
+    },
+    {
+        id:11,
+        name:'MS Dhoni',
+        image:"https://images.hindustantimes.com/img/2021/10/19/1600x900/dhoni-csk-screengrab_1634307035461_1634619555355.jpg"
+    },
+    {
+        id:12,
+        name:'Yuvraj Singh',
+        image:"https://cricketaddictor.com/wp-content/uploads/2021/05/Yuvraj-Singh.jpg"
+    },
+    {
+    id:13,
+    name:'Sachin Tendulkar',
+    image:"https://i.guim.co.uk/img/media/e48964955c4ae13e460e56a0a550097208637bbd/0_286_4365_2619/master/4365.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=782322cd3e077855b0a5cf874a79afdb"
+    },
+  ]
